72b306457690af8d2eb0782a85c81294d67698bc
ilya-t <extraletters@gmail.com>
2023-03-04 11:57:09 +0300

    fork cykedev pretty layout

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/oneday/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/oneday/keymap.c
@@ -26,13 +26,31 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_5x6_5(
-     KC_GRV,        KC_1,        KC_2,  KC_3,  KC_4,    KC_5,                                                           KC_6,   KC_7,     KC_8,         KC_9,   KC_0,           KC_MINS,
-     KC_TAB,        KC_Q,        KC_W,  KC_E,  KC_R,    KC_T,                                                           KC_Y,   KC_U,     KC_I,         KC_O,   KC_P,           KC_BSLS,
-     KC_ESC,        KC_A,        KC_S,  KC_D,  KC_F,    KC_G,                                                           KC_H,   KC_J,     KC_K,         KC_L,   KC_SCLN,        KC_QUOT,
-     OSM(MOD_LSFT), CTL_T(KC_Z), KC_X,  KC_C,  KC_V,    KC_B,                                                           KC_N,   KC_M,     KC_COMM,      KC_DOT, CTL_T(KC_SLSH), OSM(MOD_RSFT),
-                                 KC_F4,        KC_LALT, KC_BSPC, TD(TD_SYM_VIM), KC_DEL,               KC_UP,   KC_ENT, KC_SPC, OSL(VIM),               KC_EQL,
-                                                                 KC_LGUI,        TD(TD_COPY_PASTE),    KC_DOWN, OSM(MOD_RCTL)
-  ),
+  /* QWERTY
+   * .-----------------------------------------.                                  .-----------------------------------------.
+   * | Esc  |   1  |   2  |   3  |   4  |   5  |                                  |   6  |   7  |   8  |   9  |   0  |  Bsp |
+   * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |                                  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
+   * | Del  |   A  |S /CTL|D /ALT|F /CMD|   G  |                                  |   H  |J /CMD|K /ALT|L /CTL|   ;  |  '   |
+   * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
+   * | Shft |   Z  |   X  |   C  |   V  |   B  |                                  |   N  |   M  |   ,  |   .  |   /  | Shft |
+   * '-----------------------------------------/                                  \-----------------------------------------'
+   *               |      |      |    / KC_DEL/---------------.    .---------------\KC_BSPC\    |      |      |
+   *               |      |      |   /       / Space / Enter /      \ Enter \ Space \       \   |      |      |
+   *               '-------------'  '-------/ Shift /       /        \       \ Shift \-------'  '-------------'
+   *                                       /---------------/          \---------------\
+   *                                      /   =   / Home  /            \  End  \   -   \
+   *                                     /   +   /       /              \       \   _   \
+   *                                    '---------------'                '---------------'
+   */
+   KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
+   KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
+   KC_DEL , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+                     XXXXXXX, XXXXXXX,          KC_DEL , KC_SPC, KC_ENT  , KC_ENT , KC_SPC , KC_BSPC,          XXXXXXX, XXXXXXX,
+                                                         KC_EQL, KC_HOME , KC_END , KC_MINS
+   ),
 
   [SYMB] = LAYOUT_5x6_5(
      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                          KC_F6,   KC_F7,   KC_F8,    KC_F9,  KC_F10,  KC_F11,
